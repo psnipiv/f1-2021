deee28306f83a88f4daa86342c88ec40f3cdab3a
Vipin S Pillai <psnipiv@live.com>
2021-04-18 20:33:15 +0800

    Updated with latest packages

--- app.py
+++ app.py
@@ -1,12 +1,9 @@
 import streamlit as st
 import pandas as pd
-import numpy as np
 import json
 import sys
-import matplotlib.pyplot as plt
 import plotly.express as px
 import plotly.graph_objects as go
-import seaborn as sns
 import math
 
 
