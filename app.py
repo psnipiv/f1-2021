d99718575198e6d9200851dff94ad902acd3436c
Vipin S Pillai <psnipiv@live.com>
2021-05-09 23:20:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ def load_pages():
             if df_r4M.empty:
                 st.write("Session Data is not available.")
             else:
-                st.write(df_r4M.describe())                
+                #st.write(df_r4M.describe())                
                 load_toptennracefinsh(df_r4M,sectorno)
                 load_plot2(df_r4M,0,70,55,145)
                 load_plot3(df_r4M,77,100)
